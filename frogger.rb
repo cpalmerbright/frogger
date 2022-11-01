59457b0e90dbd65c95d9a1fbd22d1ebd8a77e27f
Caitlin Palmer-Bright <c_palmerbright@hotmail.com>
2022-11-01 15:55:02 +1000

    we have a road with taxis moving from left to right

--- frogger.rb
+++ frogger.rb
@@ -3,6 +3,9 @@ require 'ruby2d'
 tilesize = 50
 set width: tilesize * 9, height: tilesize * 9, background: 'navy'
 
+Rectangle.new(width: tilesize * 9, height: tilesize, x: 0, y: tilesize * 6, color: 'gray')
+taxis = []
+
 frog = Square.new(size: tilesize, x: tilesize * 4, y: tilesize * 8)
 frog.color = 'olive'
 
@@ -20,6 +23,18 @@ on :key_down do |event|
     frog.x = tilesize * 4
     frog.y = tilesize * 8
   end
+end
+
+tick = 0
+
+update do
+  taxis.each do |taxi|
+    taxi.x = taxi.x + 2
+  end
+  if tick % 120 == 0
+    taxis << Rectangle.new(width: tilesize * 2, height: tilesize, x: -tilesize * 2, y: tilesize * 6, color: 'yellow')
+  end
+  tick = tick + 1
 end
 
 show
